9debabc8c8f8b94c6f02246a49be2fcb1e5cbcaf
ZephyrIO <66318348+ZephyrIO@users.noreply.github.com>
2024-09-22 22:56:04 -0400

    created register confirm

---
+++ src/app/components/RegisterConfirm.js
@@ -0,0 +1,10 @@
+import './RegisterConfirm.css'
+
+export default function RegisterConfirm () {
+    return (
+        <div>
+            <h2>You Have Sucessfully Registered!</h2>
+            <button type="button" className="login">Login to your Account</button>
+        </div>
+    )
+}
---
+++ src/app/confirm/page.js
@@ -0,0 +1,9 @@
+import RegisterConfirm from '@/app/components/RegisterConfirm'
+
+export default function RegisterConfirmView () {
+    return (
+        <div>
+            <RegisterConfirm />
+        </div>
+    )
+}
