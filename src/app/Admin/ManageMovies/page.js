bd6beff20afcb5c3b2b92a77db91daa2eed704e9
Pranesh S <spranesh09@gmail.com>
2024-09-23 21:01:46 -0400

    remove header

--- src/app/Admin/ManageMovies/page.js
+++ src/app/Admin/ManageMovies/page.js
@@ -1,6 +1,6 @@
 'use client'
 import { useState } from 'react';
-import Header from '@/components/Header';
+//import Header from '@/components/Header';
 import Register from '@/components/ManageMovies';
 
 export default function ManageMoviesView() {
