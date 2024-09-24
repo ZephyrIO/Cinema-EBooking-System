aa203e306a7112aede0fbf472f7bda4bb4a58836
Pranesh S <spranesh09@gmail.com>
2024-09-23 23:02:45 -0400

    promo css

--- src/app/components/PromotionsCard.js
+++ src/app/components/PromotionsCard.js
@@ -4,10 +4,10 @@ import "./PromotionsCard.css";
 const PromotionCard = ({ promotion }) => {
   return (
     <div className="promotion-card">
-      <h2>{promotion.title}</h2>
-      <p>{promotion.description}</p>
-      <p>Code: <code>{promotion.code}</code></p>
-      <p>Discount: {promotion.discount}% off</p>
+      <h2 className="title">{promotion.title}</h2>
+      <p className="description">{promotion.description}</p>
+      <p className="code">Code: <code>{promotion.code}</code></p>
+      <p className="discount">Discount: {promotion.discount}% off</p>
     </div>
   );
 };
