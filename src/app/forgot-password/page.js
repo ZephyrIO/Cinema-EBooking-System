99023b910fdd691733620c33d255c54689f1b844
ZephyrIO <66318348+ZephyrIO@users.noreply.github.com>
2024-10-21 22:53:03 -0400

    fixed merge conflict

--- src/app/forgot-password/page.js
+++ src/app/forgot-password/page.js
@@ -1,18 +1,5 @@
-<<<<<<< HEAD
-'use client'
-import ResetPassword from "../components/ResetPassword";
-
-export default function ResetView() {
-    return (
-        <div>
-            <ResetPassword />
-        </div>
-    );
-}
-=======
 import ForgotPassword from '../components/ForgotPassword';
 export default function ForgotPasswordPage() {
   return <ForgotPassword />;
   
 }
->>>>>>> c1903015640b538a7385160aa650793205a5fb8b
