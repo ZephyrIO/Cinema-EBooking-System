01661a1cd19d264797e74e1d1594c6d998265c4b
ZephyrIO <66318348+ZephyrIO@users.noreply.github.com>
2024-10-21 22:43:58 -0400

    merge conflice fix

--- src/app/components/Login.js
+++ src/app/components/Login.js
@@ -69,59 +69,6 @@ export default function Login() {
     }
   };
 
-<<<<<<< HEAD
-    const handleForgot = () => {
-        router.push('/forgot-password');
-    };
-
-    const handleLogin = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await axios.post('http://localhost:3001/api/login', formData);
-            const userData = response.data;
-            console.log("updated userData:", userData);
-            console.log(response.data)
-            setUserData({
-                token: response.data.token,
-                user: response.data.user,
-            });
-            localStorage.setItem('userData', JSON.stringify(userData));
-            router.push('/');
-        } catch (err) {
-            console.error('Login failed: ', err);
-            alert(err.response.data.msg);
-        }
-    };
-
-    return (
-        <div className="login">
-            <button onClick={handleGoHome} className="back-to-home">Back to Home</button> {/* Back to Home Button */}
-            <h2 className="login-title">Login</h2>
-            <form onSubmit={handleLogin}>
-                <input
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                    required
-                    value={formData.email}
-                    onChange={handleInputChange}
-                />
-                <input
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                    required
-                    value={formData.password}
-                    onChange={handleInputChange}
-                />
-                <div className="button-group">
-                    <button type="submit" className="login">Login</button>
-                    <button type="button" className="register" onClick={handleRegister}>Register</button>
-                    <button type="button" className="forgot" onClick={handleForgot}>Forgot my Password</button>
-                </div>
-            </form>
-            {error && <p>{error}</p>}
-=======
   const handleForgotPassword = () => {
     router.push('/forgot-password'); // Navigate to the forgot password page
   };
@@ -151,7 +98,6 @@ export default function Login() {
             <button type="submit" className="login">Login</button> {/* Ensure it's type="submit" */}
             <button type="button" className="register" onClick={handleRegister}>Register</button>
             <button type="button" className="forgot" onClick={handleForgotPassword}>Forgot my Password</button>
->>>>>>> c1903015640b538a7385160aa650793205a5fb8b
         </div>
         </form>
       {error && <p>{error}</p>}
