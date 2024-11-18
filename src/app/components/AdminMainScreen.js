916fa65569cff673f702a8076b2e12e4fa515649
Pranesh S <spranesh09@gmail.com>
2024-11-17 22:30:21 -0500

    admin main screen router fix

--- src/app/components/AdminMainScreen.js
+++ src/app/components/AdminMainScreen.js
@@ -2,19 +2,44 @@
 
 import React from 'react';
 import './AdminMainScreen.css';
-import { useRouter } from 'next/navigation';
-
+import { useRouter } from 'next/navigation'; // Import useRouter
 
 export default function AdminMainScreen() {
+    const router = useRouter(); // Initialize the router
+
     return (
         <div className="adminMainScreen">
-        <h1>Admin Dashboard</h1>
-        <div className="buttons">
-          <button type="button" className="ManageMovies" onClick={() => router.push('/ManageMovies')}>Manage Movies</button>
-          <button type="button" className="ManageSchechedule" onClick={() => router.push('/ManageSchedule')}>Manage Movies Schedule</button>
-          <button type="button" className="ManageUsers" onClick={() => router.push('/ManageUsers')}>Manage Users</button>
-          <button type="button" className="ManagePromotions" onClick={() => router.push('/ManagePromotions')}>Manage Promotions</button>
+            <h1>Admin Dashboard</h1>
+            <div className="buttons">
+                <button
+                    type="button"
+                    className="ManageMovies"
+                    onClick={() => router.push('/ManageMovies')}
+                >
+                    Manage Movies
+                </button>
+                <button
+                    type="button"
+                    className="ManageSchechedule"
+                    onClick={() => router.push('/ManageSchedule')}
+                >
+                    Manage Movies Schedule
+                </button>
+                <button
+                    type="button"
+                    className="ManageUsers"
+                    onClick={() => router.push('/ManageUsers')}
+                >
+                    Manage Users
+                </button>
+                <button
+                    type="button"
+                    className="ManagePromotions"
+                    onClick={() => router.push('/ManagePromotions')}
+                >
+                    Manage Promotions
+                </button>
+            </div>
         </div>
-      </div>
     );
 }
