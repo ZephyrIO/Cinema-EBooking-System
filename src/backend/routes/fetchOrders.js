842bd06924790e2d07f51e6d75f727ea02439e34
ZephyrIO <66318348+ZephyrIO@users.noreply.github.com>
2024-12-10 20:40:46 -0500

    fixed backend issues

--- src/backend/routes/fetchOrders.js
+++ src/backend/routes/fetchOrders.js
@@ -4,10 +4,12 @@ const router = express.Router();
 
 // Route to get all orders
 router.get('/orders/:email', async (req, res) => {
-    try {
-      const orders = await Order.find({ email: req.params.email }); // Fetch all orders created by a certain user
-      res.json(orders);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  });
+  try {
+    const orders = await Order.find({ email: req.params.email }); // Fetch all orders created by a certain user
+    res.json(orders);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = router;
